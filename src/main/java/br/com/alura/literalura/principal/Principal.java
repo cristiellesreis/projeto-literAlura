4c4e8c2a8f1cae8c4f647f8afed990f444eaa999
cristiellesreis <cristielle.santos94@gmail.com>
2025-07-16 20:08:48 -0300

    feat: criando dependencias, banco de dados e menu

---
+++ src/main/java/br/com/alura/literalura/principal/Principal.java
@@ -0,0 +1,76 @@
+package br.com.alura.literalura.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+
+    public void exibeMenu() {
+
+        var opcao = -1;
+
+        while (opcao != 9) {
+            var menu = """
+                    *** Literalura ***
+                    
+                    Escolha o número de sua opção:                    
+                    
+                    1- Buscar livro pelo título
+                    2- Listar livros registrados
+                    3- Listar autores registrados
+                    4- Listar autores vivos em um determinado ano
+                    5- Listar livros em um determinado idioma
+                    
+                    9 - Sair
+                    """;
+
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    buscarLivroPeloTitulo();
+                    break;
+                case 2:
+                    listarLivrosRegistrados();
+                    break;
+                case 3:
+                    listarAutoresRegistrados();
+                    break;
+                case 4:
+                    listarAutoresVivosPorAno();
+                    break;
+                case 5:
+                    listarLivrosPorIdioma();
+                    break;
+                case 9:
+                    System.out.println("Encerrando a aplicação!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
+    }
+
+    private void listarLivrosPorIdioma() {
+    }
+
+    private void listarAutoresVivosPorAno() {
+
+    }
+
+    private void listarAutoresRegistrados() {
+
+    }
+
+    private void listarLivrosRegistrados() {
+
+    }
+
+    private void buscarLivroPeloTitulo() {
+
+    }
+
+}
